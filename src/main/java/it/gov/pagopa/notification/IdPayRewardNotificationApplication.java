bbdf3ff2157079b9e3d86b37ac4af1a459306be0
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2022-10-06 14:20:31 +0200

    IREER-162 microservice structure

---
+++ src/main/java/it/gov/pagopa/notification/IdPayRewardNotificationApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdPayRewardNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdPayRewardNotificationApplication.class, args);
+    }
+
+}

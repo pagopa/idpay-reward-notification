a298c7963b6e20193c91a450773657a18e8774df
anttorre <antonio.torre@nttdata.com>
2022-10-11 20:01:42 +0200

    fixing slow commit offset check

--- src/test/java/it/gov/pagopa/reward/notification/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/reward/notification/BaseIntegrationTest.java
@@ -177,6 +177,10 @@ public abstract class BaseIntegrationTest {
     }
 
     protected Consumer<String, String> getEmbeddedKafkaConsumer(String topic, String groupId) {
+        return getEmbeddedKafkaConsumer(topic, groupId, true);
+    }
+
+    protected Consumer<String, String> getEmbeddedKafkaConsumer(String topic, String groupId, boolean attachToBroker) {
         if (!kafkaBroker.getTopics().contains(topic)) {
             kafkaBroker.addTopics(topic);
         }
@@ -185,7 +189,9 @@ public abstract class BaseIntegrationTest {
         consumerProps.put("key.deserializer", StringDeserializer.class);
         DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         Consumer<String, String> consumer = cf.createConsumer();
-        kafkaBroker.consumeFromAnEmbeddedTopic(consumer, topic);
+        if(attachToBroker){
+            kafkaBroker.consumeFromAnEmbeddedTopic(consumer, topic);
+        }
         return consumer;
     }
 
@@ -253,7 +259,7 @@ public abstract class BaseIntegrationTest {
     }
 
     protected Map<TopicPartition, OffsetAndMetadata> getCommittedOffsets(String topic, String groupId){
-        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, groupId)) {
+        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, groupId, false)) {
             return consumer.committed(consumer.partitionsFor(topic).stream().map(p-> new TopicPartition(topic, p.partition())).collect(Collectors.toSet()));
         }
     }

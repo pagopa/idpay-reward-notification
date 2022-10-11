f648b240aea725e22c9588f080b383d86022e246
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-10-11 20:23:22 +0200

    fixing slow commit offset check (#4)
    * fixing slow commit offset check
    
    * fixed commit retry check
    
    * not attaching consumer when checking published offsets

--- src/test/java/it/gov/pagopa/reward/notification/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/reward/notification/BaseIntegrationTest.java
@@ -177,6 +177,10 @@ public abstract class BaseIntegrationTest {
     }
 
     protected Consumer<String, String> getEmbeddedKafkaConsumer(String topic, String groupId) {
+        return getEmbeddedKafkaConsumer(topic, groupId, true);
+    }
+
+    protected Consumer<String, String> getEmbeddedKafkaConsumer(String topic, String groupId, boolean attachToBroker) {
         if (!kafkaBroker.getTopics().contains(topic)) {
             kafkaBroker.addTopics(topic);
         }
@@ -185,7 +189,9 @@ public abstract class BaseIntegrationTest {
         consumerProps.put("key.deserializer", StringDeserializer.class);
         DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         Consumer<String, String> consumer = cf.createConsumer();
-        kafkaBroker.consumeFromAnEmbeddedTopic(consumer, topic);
+        if(attachToBroker){
+            kafkaBroker.consumeFromAnEmbeddedTopic(consumer, topic);
+        }
         return consumer;
     }
 
@@ -253,7 +259,7 @@ public abstract class BaseIntegrationTest {
     }
 
     protected Map<TopicPartition, OffsetAndMetadata> getCommittedOffsets(String topic, String groupId){
-        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, groupId)) {
+        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, groupId, false)) {
             return consumer.committed(consumer.partitionsFor(topic).stream().map(p-> new TopicPartition(topic, p.partition())).collect(Collectors.toSet()));
         }
     }
@@ -274,8 +280,8 @@ public abstract class BaseIntegrationTest {
                 final Map<TopicPartition, OffsetAndMetadata> commits = getCommittedOffsets(topic, groupId);
                 Assertions.assertEquals(expectedCommittedMessages, commits.values().stream().mapToLong(OffsetAndMetadata::offset).sum());
                 return commits;
-            } catch (RuntimeException e){
-                lastException = e;
+            } catch (Throwable e){
+                lastException = new RuntimeException(e);
                 wait(millisAttemptDelay, TimeUnit.MILLISECONDS);
             }
         }
@@ -283,7 +289,7 @@ public abstract class BaseIntegrationTest {
     }
 
     protected Map<TopicPartition, Long> getEndOffsets(String topic){
-        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, "idpay-group-test-check")) {
+        try (Consumer<String, String> consumer = getEmbeddedKafkaConsumer(topic, "idpay-group-test-check", false)) {
             return consumer.endOffsets(consumer.partitionsFor(topic).stream().map(p-> new TopicPartition(topic, p.partition())).toList());
         }
     }

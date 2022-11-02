cbdfde72059cccc11d271887b53fa39a532940c0
anttorre <antonio.torre@nttdata.com>
2022-11-02 16:15:42 +0100

    storing updateDateTime

--- src/main/java/it/gov/pagopa/reward/notification/model/RewardNotificationRule.java
+++ src/main/java/it/gov/pagopa/reward/notification/model/RewardNotificationRule.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -22,4 +23,5 @@ public class RewardNotificationRule {
     private AccumulatedAmountDTO accumulatedAmount;
     private TimeParameterDTO timeParameter;
     private String organizationFiscalCode;
+    private LocalDateTime updateDate;
 }

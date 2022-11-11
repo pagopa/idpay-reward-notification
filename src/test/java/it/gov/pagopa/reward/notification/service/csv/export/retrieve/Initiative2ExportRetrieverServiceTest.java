cc1d2eaefee3a41ac0a5ce4e8b0f4a14e182fd44
anttorre <antonio.torre@nttdata.com>
2022-11-11 15:16:11 +0100

    fixed unitTests

--- src/test/java/it/gov/pagopa/reward/notification/service/csv/export/retrieve/Initiative2ExportRetrieverServiceTest.java
+++ src/test/java/it/gov/pagopa/reward/notification/service/csv/export/retrieve/Initiative2ExportRetrieverServiceTest.java
@@ -250,7 +250,7 @@ class Initiative2ExportRetrieverServiceTest {
 
         String todayStr=Utils.FORMATTER_DATE.format(LocalDate.now());
         Assertions.assertEquals("ID_0_ssx_%s.12".formatted(todayStr), result.getId());
-        Assertions.assertEquals("/export/base/path/ORGANIZATION_ID_0_hpd/ID_0_ssx/NAME_0_vnj_%s.12.zip".formatted(todayStr), result.getFilePath());
+        Assertions.assertEquals("ORGANIZATION_ID_0_hpd/ID_0_ssx/export/NAME_0_vnj_%s.12.zip".formatted(todayStr), result.getFilePath());
         Assertions.assertEquals(baseExport.getInitiativeId(), result.getInitiativeId());
         Assertions.assertEquals(baseExport.getInitiativeName(), result.getInitiativeName());
         Assertions.assertEquals(baseExport.getOrganizationId(), result.getOrganizationId());

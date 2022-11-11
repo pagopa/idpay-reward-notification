1d21bc6f5034ec55c0997996ab69e7008a3c52d0
anttorre <antonio.torre@nttdata.com>
2022-11-11 10:05:06 +0100

    uploading Azure using blocking API

--- src/main/java/it/gov/pagopa/reward/notification/azure/storage/AzureBlobClientImpl.java
+++ src/main/java/it/gov/pagopa/reward/notification/azure/storage/AzureBlobClientImpl.java
@@ -1,28 +1,51 @@
 package it.gov.pagopa.reward.notification.azure.storage;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
 
 @Slf4j
 @Service
 public class AzureBlobClientImpl implements AzureBlobClient {
+
+    private final String blobContainerName;
+    private final CloudBlobClient blobClient;
+
+    public AzureBlobClientImpl(
+            @Value("${app.csv.export.storage.connection-string}") String storageConnectionString,
+            @Value("${app.csv.export.storage.blob-container-name}") String blobContainerName)
+            throws URISyntaxException, InvalidKeyException {
+        final CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+        this.blobClient = storageAccount.createCloudBlobClient();
+        this.blobContainerName = blobContainerName;
+    }
+
     @Override
-    public Mono<File> uploadFile(File file, String destination, String contentType) {// TODO remove this mock and implement it
-        log.info("Uploading file {} (contentType{}) into azure blob at destination {}", file.getName(), contentType, destination);
-        Path zipPath = Path.of(file.getAbsolutePath());
+    public Mono<File> uploadFile(File file, String destination, String contentType) {
+        log.info("Uploading file {} (contentType={}) into azure blob at destination {}", file.getName(), contentType, destination);
         try {
-            Files.copy(zipPath,
-                    zipPath.getParent().resolve(zipPath.getFileName().toString().replace(".zip", ".uploaded.zip")),
-                    StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
+            final CloudBlobContainer blobContainer = blobClient.getContainerReference(blobContainerName);
+            final CloudBlockBlob blob = blobContainer.getBlockBlobReference(destination);
+            blob.getProperties().setContentType(contentType);
+            blob.uploadFromFile(file.getAbsolutePath());
+            log.info("File uploaded {} (contentType={}) into azure blob at destination {}", file.getName(), contentType, destination);
+
+        } catch (StorageException | URISyntaxException | IOException e) {
+            throw new IllegalStateException(
+                    "Cannot upload file file file %s (contentType=%s) into azure blob at destination %s"
+                            .formatted(file.getName(), contentType, destination),
+                    e);
         }
         return Mono.just(file);
     }

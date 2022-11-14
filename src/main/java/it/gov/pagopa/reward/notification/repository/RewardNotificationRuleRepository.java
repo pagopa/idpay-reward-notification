f7191f8d694c9801e9cf2269ead84dc97b219563
pelliccm <matteo.pelliccione.sa@nttdata.com>
2022-11-14 19:17:33 +0100

    IGR-38 RewardsNotificationExpiredInitiativeHandlerService

--- src/main/java/it/gov/pagopa/reward/notification/repository/RewardNotificationRuleRepository.java
+++ src/main/java/it/gov/pagopa/reward/notification/repository/RewardNotificationRuleRepository.java
@@ -1,7 +1,13 @@
 package it.gov.pagopa.reward.notification.repository;
 
+import it.gov.pagopa.reward.notification.dto.rule.AccumulatedAmountDTO;
 import it.gov.pagopa.reward.notification.model.RewardNotificationRule;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
 
 public interface RewardNotificationRuleRepository extends ReactiveMongoRepository<RewardNotificationRule, String> {
+
+    Flux<RewardNotificationRule> findByAccumulatedAmountIsNotAndEndDateGreaterThanEqualsAndEndDateLessThan(AccumulatedAmountDTO accumulatedAmount, LocalDate from, LocalDate to);
 }

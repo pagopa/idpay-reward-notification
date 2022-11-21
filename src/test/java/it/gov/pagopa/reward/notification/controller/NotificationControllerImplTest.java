0b51adb07096eb093e679ed663626464cf1c859d
pelliccm <matteo.pelliccione.sa@nttdata.com>
2022-11-21 15:57:12 +0100

    IGR-57 API

--- src/test/java/it/gov/pagopa/reward/notification/controller/NotificationControllerImplTest.java
+++ src/test/java/it/gov/pagopa/reward/notification/controller/NotificationControllerImplTest.java
@@ -2,11 +2,15 @@ package it.gov.pagopa.reward.notification.controller;
 
 import it.gov.pagopa.reward.notification.config.JsonConfig;
 import it.gov.pagopa.reward.notification.dto.controller.ExportFilter;
+import it.gov.pagopa.reward.notification.dto.controller.ImportFilter;
 import it.gov.pagopa.reward.notification.dto.controller.RewardExportsDTO;
+import it.gov.pagopa.reward.notification.dto.controller.RewardImportsDTO;
 import it.gov.pagopa.reward.notification.model.RewardOrganizationExport;
 import it.gov.pagopa.reward.notification.service.csv.out.ExportRewardNotificationCsvService;
 import it.gov.pagopa.reward.notification.service.exports.OrganizationExportsServiceImpl;
+import it.gov.pagopa.reward.notification.service.imports.OrganizationImportsServiceImpl;
 import it.gov.pagopa.reward.notification.test.fakers.RewardExportsDTOFaker;
+import it.gov.pagopa.reward.notification.test.fakers.RewardImportsDTOFaker;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +36,8 @@ class NotificationControllerImplTest {
     private OrganizationExportsServiceImpl organizationExportsServiceMock;
     @MockBean
     private ExportRewardNotificationCsvService exportRewardNotificationCsvServiceMock;
+    @MockBean
+    private OrganizationImportsServiceImpl organizationImportsServiceMock;
 
     @Autowired
     protected WebTestClient webClient;
@@ -134,5 +140,89 @@ class NotificationControllerImplTest {
                 .isEqualTo(pageMock);
 
         Mockito.verify(organizationExportsServiceMock, Mockito.times(1)).findAllPaged("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ExportFilter());
+    }
+
+    @Test
+    void testGetImportsOk() {
+        RewardImportsDTO rewardImportsDTO = RewardImportsDTOFaker.mockInstance(1);
+
+        Mockito.when(organizationImportsServiceMock.findAllBy("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter()))
+                .thenReturn(Flux.just(rewardImportsDTO));
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/idpay/organization/{organizationId}/initiative/{initiativeId}/reward/notification/imports")
+                        .build(rewardImportsDTO.getOrganizationId(), rewardImportsDTO.getInitiativeId()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(RewardImportsDTO.class).isEqualTo(List.of(rewardImportsDTO));
+
+        Mockito.verify(organizationImportsServiceMock, Mockito.times(1)).findAllBy("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter());
+    }
+
+    @Test
+    void testGetImportsEmpty() {
+        RewardImportsDTO rewardImportsDTO = RewardImportsDTOFaker.mockInstance(1);
+        Mockito.when(organizationImportsServiceMock.findAllBy("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter()))
+                .thenReturn(Flux.empty());
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/idpay/organization/{organizationId}/initiative/{initiativeId}/reward/notification/imports")
+                        .build(rewardImportsDTO.getOrganizationId(), rewardImportsDTO.getInitiativeId()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(RewardExportsDTO.class).isEqualTo(Collections.emptyList());
+
+        Mockito.verify(organizationImportsServiceMock, Mockito.times(1)).findAllBy("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter());
+    }
+
+    @Test
+    void testGetImportsCountOk() {
+        Long count = 1L;
+        Mockito.when(organizationImportsServiceMock.countAll("ORGANIZATION_ID_1", "INITIATIVE_ID_1", new ImportFilter()))
+                .thenReturn(Mono.just(count));
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/idpay/organization/{organizationId}/initiative/{initiativeId}/reward/notification/imports/count")
+                        .build("ORGANIZATION_ID_1", "INITIATIVE_ID_1"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Long.class).isEqualTo(count);
+
+        Mockito.verify(organizationImportsServiceMock, Mockito.times(1)).countAll("ORGANIZATION_ID_1", "INITIATIVE_ID_1", new ImportFilter());
+    }
+
+    @Test
+    void testGetImportsCountEmpty() {
+        Mockito.when(organizationImportsServiceMock.countAll("ORGANIZATION_ID_1", "INITIATIVE_ID_1", new ImportFilter()))
+                .thenReturn(Mono.empty());
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/idpay/organization/{organizationId}/initiative/{initiativeId}/reward/notification/imports/count")
+                        .build("ORGANIZATION_ID_1", "INITIATIVE_ID_1"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Long.class).isEqualTo(0L);
+
+        Mockito.verify(organizationImportsServiceMock, Mockito.times(1)).countAll("ORGANIZATION_ID_1", "INITIATIVE_ID_1", new ImportFilter());
+    }
+
+    @Test
+    void testGetImportsPaged() {
+        RewardImportsDTO rewardImportsDTO = RewardImportsDTOFaker.mockInstance(1);
+        PageImpl<RewardImportsDTO> pageMock = new PageImpl<>(List.of(rewardImportsDTO), TEST_PAGE_REQUEST, 1);
+
+
+        Mockito.when(organizationImportsServiceMock.findAllPaged("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter()))
+                .thenReturn(Mono.just(pageMock));
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/idpay/organization/{organizationId}/initiative/{initiativeId}/reward/notification/imports/paged")
+                        .build("ORGANIZATION_ID_1", "INITIATIVE_ID_1"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Page<RewardImportsDTO>>() {})
+                .isEqualTo(pageMock);
+
+        Mockito.verify(organizationImportsServiceMock, Mockito.times(1)).findAllPaged("ORGANIZATION_ID_1", "INITIATIVE_ID_1", TEST_PAGE_REQUEST, new ImportFilter());
     }
 }
---
+++ src/test/java/it/gov/pagopa/reward/notification/test/fakers/RewardImportsDTOFaker.java
@@ -0,0 +1,58 @@
+package it.gov.pagopa.reward.notification.test.fakers;
+
+import com.github.javafaker.service.FakeValuesService;
+import it.gov.pagopa.reward.notification.dto.controller.RewardImportsDTO;
+import it.gov.pagopa.reward.notification.enums.RewardOrganizationImportStatus;
+import it.gov.pagopa.reward.notification.test.utils.TestUtils;
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class RewardImportsDTOFaker {
+    public static final LocalDateTime CHOSEN_DATE = LocalDateTime.now();
+
+    /**
+     * It will return an example of {@link RewardImportsDTO}. Providing a bias, it will return a pseudo-casual object
+     */
+    public static RewardImportsDTO mockInstance(Integer bias) {
+        return mockInstanceBuilder(bias).build();
+    }
+
+    /**
+     * It will return an example of builder to obtain a {@link RewardImportsDTO}. Providing a bias, it will return a pseudo-casual object
+     */
+    public static RewardImportsDTO.RewardImportsDTOBuilder mockInstanceBuilder(Integer bias) {
+        RewardImportsDTO.RewardImportsDTOBuilder out = RewardImportsDTO.builder();
+
+        bias = ObjectUtils.firstNonNull(bias, InitiativeRefundDTOFaker.getRandomPositiveNumber(null));
+
+        FakeValuesService fakeValuesService = InitiativeRefundDTOFaker.getFakeValuesService(bias);
+
+        out.filePath("path%d".formatted(bias));
+        out.initiativeId("INITIATIVE_ID_%d".formatted(bias));
+        out.organizationId("ORGANIZATION_ID_%d".formatted(bias));
+        out.feedbackDate(CHOSEN_DATE);
+        out.eTag("ETAG");
+        out.contentLength(bias);
+        out.url("URL_%d".formatted(bias));
+
+        out.rewardsResulted(bias + 1L);
+        out.rewardsResultedError(0L);
+        out.rewardsResultedOk(bias.longValue());
+        out.rewardsResultedOkError(0L);
+
+        out.percentageResulted("%d".formatted((bias + 1L) * 1000L));
+        out.percentageResultedOk("%d".formatted(bias * 1000L));
+        out.percentageResultedOkElab("%d".formatted(bias * 1000L));
+
+        out.elabDate(CHOSEN_DATE);
+        out.exportIds(new ArrayList<>());
+        out.status(RewardOrganizationImportStatus.COMPLETE);
+
+        out.errorsSize(0);
+
+        TestUtils.checkNotNullFields(out);
+        return out;
+    }
+}

7775831faecea55faeae60638a7844a685e50347
Benedetta-fabbri <Benedetta11.Fabbri@emea.nttdata.com>
2023-02-15 16:33:13 +0100

    [IDP-233] new Audit logs added

--- src/test/java/it/gov/pagopa/reward/notification/service/csv/out/writer/ExportCsvFinalizeServiceTest.java
+++ src/test/java/it/gov/pagopa/reward/notification/service/csv/out/writer/ExportCsvFinalizeServiceTest.java
@@ -11,6 +11,7 @@ import it.gov.pagopa.reward.notification.enums.RewardOrganizationExportStatus;
 import it.gov.pagopa.reward.notification.model.RewardOrganizationExport;
 import it.gov.pagopa.reward.notification.repository.RewardOrganizationExportsRepository;
 import it.gov.pagopa.reward.notification.repository.RewardsNotificationRepository;
+import it.gov.pagopa.reward.notification.utils.Utilities;
 import it.gov.pagopa.reward.notification.utils.ZipUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.junit.jupiter.api.Assertions;
@@ -39,14 +40,14 @@ class ExportCsvFinalizeServiceTest {
     @Mock private RewardsNotificationRepository rewardsNotificationRepositoryMock;
     @Mock private RewardOrganizationExportsRepository rewardOrganizationExportsRepositoryMock;
     @Mock private RewardsNotificationBlobClient rewardsNotificationBlobClientMock;
-
+    @Mock private Utilities utilitiesMock;
     private ExportCsvFinalizeService service;
 
     @BeforeEach
     void init() {
         ((Logger) LoggerFactory.getLogger("org.apache.commons.beanutils.converters")).setLevel(Level.OFF);
         char csvSeparator = ';';
-        service = new ExportCsvFinalizeServiceImpl("target/tmp", csvSeparator, rewardsNotificationRepositoryMock, rewardOrganizationExportsRepositoryMock, rewardsNotificationBlobClientMock);
+        service = new ExportCsvFinalizeServiceImpl("target/tmp", csvSeparator, rewardsNotificationRepositoryMock, rewardOrganizationExportsRepositoryMock, rewardsNotificationBlobClientMock, utilitiesMock);
     }
 
     @Test

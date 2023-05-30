560ceacda970eaf354cab09574f4e3ce55911245
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-05-30 18:17:46 +0200

    IDP-1106 findById optimization for cosmosDb (#113)

--- src/test/java/it/gov/pagopa/reward/notification/connector/azure/storage/RewardsNotificationBlobClientTestIntegrated.java
+++ src/test/java/it/gov/pagopa/reward/notification/connector/azure/storage/RewardsNotificationBlobClientTestIntegrated.java
@@ -6,6 +6,10 @@ import java.io.*;
 import java.nio.file.Path;
 import java.util.Properties;
 
+/**
+ * See confluence page: <a href="https://pagopa.atlassian.net/wiki/spaces/IDPAY/pages/615974424/Secrets+UnitTests">Secrets for UnitTests</a>
+ */
+@SuppressWarnings({"squid:S3577", "NewClassNamingConvention"}) // suppressing class name not match alert: we are not using the Test suffix in order to let not execute this test by default maven configuration because it depends on properties not pushable. See
 class RewardsNotificationBlobClientTestIntegrated extends RewardsNotificationBlobClientTest {
 
     private final String connectionString;

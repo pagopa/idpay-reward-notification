51fa8d317e99086f1ebc005fab2bf0fb79c46362
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2023-11-16 16:51:24 +0100

    IDP-1790-5-sp-dettaglio-refund-risponde-401 fix mapper getRefund

--- src/main/java/it/gov/pagopa/reward/notification/dto/mapper/detail/RewardsNotification2DetailDTOMapper.java
+++ src/main/java/it/gov/pagopa/reward/notification/dto/mapper/detail/RewardsNotification2DetailDTOMapper.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.reward.notification.dto.mapper.detail;
 
 import it.gov.pagopa.common.utils.CommonUtilities;
 import it.gov.pagopa.reward.notification.dto.controller.detail.RewardNotificationDetailDTO;
+import it.gov.pagopa.reward.notification.enums.BeneficiaryType;
 import it.gov.pagopa.reward.notification.model.RewardsNotification;
 import it.gov.pagopa.reward.notification.utils.Utils;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class RewardsNotification2DetailDTOMapper {
         return RewardNotificationDetailDTO.builder()
                 .id(notification.getId())
                 .externalId(notification.getExternalId())
-                .userId(notification.getBeneficiaryId())
+                .userId(notification.getBeneficiaryType().equals(BeneficiaryType.CITIZEN) ? notification.getBeneficiaryId() : notification.getMerchantFiscalCode())
                 .iban(notification.getIban())
                 .amount(CommonUtilities.centsToEuro(notification.getRewardCents()))
                 .startDate(notification.getStartDepositDate())

f35a0db2163ccf07f43880bd2bcebdbb1c02ca72
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-12-21 11:00:03 +0100

    feat: IDP-2075 new error codes (#193)
    * feat: IDP-2075 new Error Codes
    
    * bump version
    
    * fixed unitTests

--- src/test/java/it/gov/pagopa/common/reactive/web/exception/MongoExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/common/reactive/web/exception/MongoExceptionHandlerTest.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.common.reactive.web.exception;
 
-import static org.mockito.Mockito.doThrow;
-
 import com.mongodb.MongoQueryException;
 import com.mongodb.MongoWriteException;
 import com.mongodb.ServerAddress;
@@ -27,61 +25,65 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.mockito.Mockito.doThrow;
+
 @ExtendWith(SpringExtension.class)
 @WebFluxTest
 @ContextConfiguration(classes = {MongoExceptionHandler.class,
-    MongoExceptionHandlerTest.TestController.class, ErrorManager.class})
+        MongoExceptionHandlerTest.TestController.class, ErrorManager.class})
 class MongoExceptionHandlerTest {
 
-  @Autowired
-  private WebTestClient webTestClient;
+    public static final ErrorDTO EXPECTED_DEFAULT_ERROR = new ErrorDTO("Error", "Something gone wrong");
+    public static final ErrorDTO EXPECTED_TOO_MANY_REQUESTS_ERROR = new ErrorDTO("TOO_MANY_REQUESTS", "Too Many Requests");
 
-  @SpyBean
-  private TestController testControllerSpy;
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @SpyBean
+    private TestController testControllerSpy;
 
 
-  @RestController
-  @Slf4j
-  static class TestController {
+    @RestController
+    @Slf4j
+    static class TestController {
 
-    @GetMapping("/test")
-    String testEndpoint() {
-      return "OK";
+        @GetMapping("/test")
+        String testEndpoint() {
+            return "OK";
+        }
     }
-  }
 
 
-  @Test
-  void handleUncategorizedMongoDbException() {
+    @Test
+    void handleUncategorizedMongoDbException() {
 
-    String mongoFullErrorResponse = """
+        String mongoFullErrorResponse = """
         {"ok": 0.0, "errmsg": "Error=16500, RetryAfterMs=34,\s
         Details='Response status code does not indicate success: TooManyRequests (429) Substatus: 3200 ActivityId: 46ba3855-bc3b-4670-8609-17e1c2c87778 Reason:\s
         (\\r\\nErrors : [\\r\\n \\"Request rate is large. More Request Units may be needed, so no changes were made. Please retry this request later. Learn more:
          http://aka.ms/cosmosdb-error-429\\"\\r\\n]\\r\\n) ", "code": 16500, "codeName": "RequestRateTooLarge"}
         """;
 
-    final MongoQueryException mongoQueryException = new MongoQueryException(
-        BsonDocument.parse(mongoFullErrorResponse), new ServerAddress());
-    doThrow(
-        new UncategorizedMongoDbException(mongoQueryException.getMessage(), mongoQueryException))
-        .when(testControllerSpy).testEndpoint();
-    ErrorDTO expectedErrorDefault = new ErrorDTO("TOO_MANY_REQUESTS","TOO_MANY_REQUESTS");
+        final MongoQueryException mongoQueryException = new MongoQueryException(
+                BsonDocument.parse(mongoFullErrorResponse), new ServerAddress());
+        doThrow(
+                new UncategorizedMongoDbException(mongoQueryException.getMessage(), mongoQueryException))
+                .when(testControllerSpy).testEndpoint();
 
-    webTestClient.get()
-        .uri(uriBuilder -> uriBuilder.path("/test").build())
-        .exchange()
-        .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
-        .expectHeader().exists(HttpHeaders.RETRY_AFTER)
-        .expectHeader().valueEquals(HttpHeaders.RETRY_AFTER, "1")
-        .expectHeader().valueEquals("Retry-After-Ms", "34")
-        .expectBody(ErrorDTO.class).isEqualTo(expectedErrorDefault);
-  }
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/test").build())
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
+                .expectHeader().exists(HttpHeaders.RETRY_AFTER)
+                .expectHeader().valueEquals(HttpHeaders.RETRY_AFTER, "1")
+                .expectHeader().valueEquals("Retry-After-Ms", "34")
+                .expectBody(ErrorDTO.class).isEqualTo(EXPECTED_TOO_MANY_REQUESTS_ERROR);
+    }
 
-  @Test
-  void handleTooManyWriteDbException() {
+    @Test
+    void handleTooManyWriteDbException() {
 
-    String writeErrorMessage = """
+        String writeErrorMessage = """
             Error=16500, RetryAfterMs=34, Details='Response status code does not indicate success: TooManyRequests (429); Substatus: 3200; ActivityId: 822d212d-5aac-4f5d-a2d4-76d6da7b619e; Reason: (
             Errors : [
               "Request rate is large. More Request Units may be needed, so no changes were made. Please retry this request later. Learn more: http://aka.ms/cosmosdb-error-429"
@@ -89,49 +91,44 @@ class MongoExceptionHandlerTest {
             );
             """;
 
-    final MongoWriteException mongoWriteException = new MongoWriteException(
+        final MongoWriteException mongoWriteException = new MongoWriteException(
             new WriteError(16500, writeErrorMessage, BsonDocument.parse("{}")), new ServerAddress());
-    doThrow(
+        doThrow(
             new DataIntegrityViolationException(mongoWriteException.getMessage(), mongoWriteException))
             .when(testControllerSpy).testEndpoint();
-    ErrorDTO expectedErrorDefault = new ErrorDTO("TOO_MANY_REQUESTS","TOO_MANY_REQUESTS");
 
-    webTestClient.get()
+        webTestClient.get()
             .uri(uriBuilder -> uriBuilder.path("/test").build())
             .exchange()
             .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
             .expectHeader().exists(HttpHeaders.RETRY_AFTER)
             .expectHeader().valueEquals(HttpHeaders.RETRY_AFTER, "1")
             .expectHeader().valueEquals("Retry-After-Ms", "34")
-            .expectBody(ErrorDTO.class).isEqualTo(expectedErrorDefault);
-  }
+            .expectBody(ErrorDTO.class).isEqualTo(EXPECTED_TOO_MANY_REQUESTS_ERROR);
+    }
 
-  @Test
-  void handleUncategorizedMongoDbExceptionNotRequestRateTooLarge() {
+    @Test
+    void handleUncategorizedMongoDbExceptionNotRequestRateTooLarge() {
 
-    doThrow(new UncategorizedMongoDbException("DUMMY", new Exception()))
-        .when(testControllerSpy).testEndpoint();
+        doThrow(new UncategorizedMongoDbException("DUMMY", new Exception()))
+                .when(testControllerSpy).testEndpoint();
 
-    ErrorDTO expectedErrorDefault = new ErrorDTO("Error","Something gone wrong");
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/test").build())
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+                .expectBody(ErrorDTO.class).isEqualTo(EXPECTED_DEFAULT_ERROR);
+    }
 
-    webTestClient.get()
-        .uri(uriBuilder -> uriBuilder.path("/test").build())
-        .exchange()
-        .expectStatus().isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
-        .expectBody(ErrorDTO.class).isEqualTo(expectedErrorDefault);
-  }
+    @Test
+    void handleMongoRequestRateTooLargeRetryExpiredException() {
+        doThrow(new MongoRequestRateTooLargeRetryExpiredException("FLOWNAME",3,3,0,100,34L,new Exception()))
+                .when(testControllerSpy).testEndpoint();
 
-  @Test
-  void handleMongoRequestRateTooLargeRetryExpiredException() {
-    doThrow(new MongoRequestRateTooLargeRetryExpiredException("FLOWNAME",3,3,0,100,34L,new Exception()))
-        .when(testControllerSpy).testEndpoint();
-
-    ErrorDTO expectedErrorDefault = new ErrorDTO("TOO_MANY_REQUESTS","TOO_MANY_REQUESTS");
-
-    webTestClient.get()
-        .uri(uriBuilder -> uriBuilder.path("/test").build())
-        .exchange()
-        .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
-        .expectBody(ErrorDTO.class).isEqualTo(expectedErrorDefault);
-  }
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/test").build())
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
+                .expectBody(ErrorDTO.class).isEqualTo(EXPECTED_TOO_MANY_REQUESTS_ERROR);
+    }
 }
